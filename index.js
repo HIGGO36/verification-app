3b4cd28f4c6da946bd174f5f5a68b7e334114d96
John D. Higgins <stuffadmin@protonmail.com>
2024-02-20 20:38:07 -0500

    removed unneeded dependencies

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 require('dotenv').config();
 
 const express = require('express');
@@ -7,6 +6,7 @@ const helmet = require('helmet');
 const fs = require('fs');
 const admin = require('firebase-admin');
 const userSignup = require('./utils/userSignup');
+const secureCookies = require('./middleware/secureCookies');
 const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
 let serviceAccount = serviceAccountPath ? JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8')) : {};
 
@@ -26,13 +26,13 @@ app.use(express.json());
 app.use(cors());
 app.use(rateLimiting);
 app.use(sanitizeRequestBody);
-app.set('trust proxy', 1);
+app.set('trust proxy', 1); // Important for secureCookies to correctly determine if the request is secure
+app.use(secureCookies); // Use secureCookies middleware
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Email Verification Service');
 });
 
-// Use validateResult here correctly as imported
 app.post('/signup', validateSignUp, validateResult, async (req, res) => {
     try {
         const userId = await userSignup(req.body);
@@ -48,4 +48,6 @@ app.post('/signup', validateSignUp, validateResult, async (req, res) => {
 });
 
 app.post('/verify-email', verifyEmailDomain);
+
+// Remove the duplicate app.listen call
 app.listen(port, () => console.log(`Server running on port ${port}`));
